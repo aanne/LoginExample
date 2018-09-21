e05fd50589b84417349d56f3e757bb9585ef2870
charlie <526825550@qq.com>
2018-09-21 11:38:12 +0800

    first commit

---
+++ WebContent/LoginSuccess.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+<title>Login success page</title>
+</head>
+<body>
+<h3>Hi Charlie, Login successful! </h3>
+<a href="Login.html">Login Page</a>
+</body>
+</html>
---
+++ src/com/charlie/servlet/LoginServlet.java
@@ -0,0 +1,46 @@
+
+package com.charlie.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+		description = "Login Servlet", 
+		urlPatterns = { "/LoginServlet" }, 
+		initParams = { 
+				@WebInitParam(name = "user", value = "charlie"), 
+				@WebInitParam(name = "password", value = "yiwen")
+		})
+
+//first commit annotation
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+		
+		String userID = getServletConfig().getInitParameter("user");
+		String password = getServletConfig().getInitParameter("password");
+	
+		log("User="+user+",password="+pwd);
+		
+		if(userID.equals(user) && password.equals(pwd)){
+			response.sendRedirect("LoginSuccess.jsp");
+		}else{
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+			PrintWriter out= response.getWriter();
+			out.println("<font color=red>Either user name or password is wrong.</font>");
+			rd.include(request, response);
+		}
+	}
+}
